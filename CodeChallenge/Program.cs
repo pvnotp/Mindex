5f8d1804b89a668335747389c76f8fb87dceaa2c
Alison Forster <alisonjoyforster@gmail.com>
2025-01-10 02:14:45 -0500

    Add project files.

---
+++ CodeChallenge/Program.cs
@@ -0,0 +1,17 @@
+﻿using CodeChallenge.Config;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        //var builder = WebApplication.CreateBuilder(z);
+        //builder.Services.AddSwaggerGen();
+        //var app = builder.Build();
+        //app.UseSwagger();
+        //app.UseSwaggerUI();
+        new App().Configure(args).Run();
+    }
+}
