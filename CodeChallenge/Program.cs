b941fdc415b29a76cc8310798ea1012ba1036b5f
Alison Forster <alisonjoyforster@gmail.com>
2025-01-10 02:15:24 -0500

    Initial commit

--- CodeChallenge/Program.cs
+++ CodeChallenge/Program.cs
@@ -7,11 +7,6 @@ public class Program
 
     public static void Main(string[] args)
     {
-        //var builder = WebApplication.CreateBuilder(z);
-        //builder.Services.AddSwaggerGen();
-        //var app = builder.Build();
-        //app.UseSwagger();
-        //app.UseSwaggerUI();
         new App().Configure(args).Run();
     }
 }
